9ac510921cdadc7f3ab7bd2957a83765153e6c71
Bhavani Shankar <113505769+Shankar1006@users.noreply.github.com>
2022-09-19 00:00:17 +0530

    Update person.service.ts

--- src/app/service/person.service.ts
+++ src/app/service/person.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Person } from '../modal/person.modal';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
